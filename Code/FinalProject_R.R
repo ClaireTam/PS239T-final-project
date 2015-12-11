9f209a1173dca7899b6f31914445c642de2a0b6d
Claire Tam <claire.tam@berkeley.edu>
2015-12-10 16:46:23 -0800

    saving pdfs

--- Code/FinalProject_R.R
+++ Code/FinalProject_R.R
@@ -1,5 +1,5 @@
 # set working directory and clear environment
-setwd("~/Desktop/Berkeley/Classes15_16/PS23T-final-project")
+setwd("~/Desktop/Berkeley/Classes15_16/PS239T-final-project/Data")
 rm(list = ls())
 
 # load packages 
@@ -20,13 +20,29 @@ AIF$Year <- substr(as.character(AIF$Date), 1, 4)
 Iraq_map <- get_map('Iraq', zoom = 6)
 ggmap(Iraq_map) +
   geom_point(data=AIF, aes(x=Longitude, y=Latitude, color = factor(Year)), size = 2) +
-  scale_color_manual(values=c("deeppink", "darkorchid4", "red3", "chartreuse4", "chocolate4", "darkslategray"))
+  scale_color_manual(values=c("deeppink", "darkorchid4", "red3", "chartreuse4", "chocolate4", "darkslategray"))+
+  ggtitle("AIF Attacks")
 
-# Zoomed In Map
-Iraq_map_Zoom <- get_googlemap(center = c(lat = 44.3, lon = 33.3), zoom = 10)
-ggmap(Iraq_map_Zoom) + 
+ggsave ("~/Desktop/Berkeley/Classes15_16/PS239T-final-project/Results/Iraqmap_Overview.pdf", dpi = 200)
+dev.off()
+
+# Zoomed In Map 1
+Iraq_map_Zoom_1<- get_googlemap(center = c(lat = 44.3, lon = 33.08), zoom = 10)
+ggmap(Iraq_map_Zoom_1) + 
   geom_point(data=AIF, aes(x=Longitude, y=Latitude, color = factor(Year)), size = 3) +
-  scale_color_manual(values=c("deeppink", "darkorchid4", "red3", "chartreuse4", "chocolate4", "darkslategray"))
+  scale_color_manual(values=c("deeppink", "darkorchid4", "red3", "chartreuse4", "chocolate4", "darkslategray")) +
+  ggtitle("AIF Attacks Detail - Baghdad Area")
+ggsave ("~/Desktop/Berkeley/Classes15_16/PS239T-final-project/Results/Iraqmap_Baghdad.pdf", dpi = 200)
+dev.off()
+
+# Zoomed In Map 2
+Iraq_map_Zoom_2 <- get_googlemap(center = c(lat = 44.35, lon = 35.47), zoom = 12)
+ggmap(Iraq_map_Zoom_2) + 
+  geom_point(data=AIF, aes(x=Longitude, y=Latitude, color = factor(Year)), size = 3) +
+  scale_color_manual(values=c("deeppink", "darkorchid4", "red3", "chartreuse4", "chocolate4", "darkslategray")) +
+  ggtitle("AIF Attacks Detail - Kirkuk")
+ggsave ("~/Desktop/Berkeley/Classes15_16/PS239T-final-project/Results/Iraqmap_Kirkuk.pdf", dpi = 200)
+dev.off()
 
 # Create barplot examining count attack per year and month
 year_month <- AIF$Date
@@ -38,6 +54,8 @@ ggplot(data = year_month_freq, aes(x = year_month, y = Freq )) + geom_bar(stat =
   theme(axis.text=element_text(size=14),
         axis.title=element_text(size=14,face="bold"),
         plot.title = element_text(face = "bold", size = 16))
+ggsave ("~/Desktop/Berkeley/Classes15_16/PS239T-final-project/Results/Month_Year_Graph.pdf", dpi = 200)
+dev.off()
 
 # Create barplot examining count attack per year
 year_df <- as.data.frame(table(AIF$Year))
@@ -48,3 +66,5 @@ ggplot(data = year_df, aes(x = Var1, y = Freq)) + geom_bar(stat = "identity") +
   theme(axis.text=element_text(size=14),
         axis.title=element_text(size=14,face="bold"),
         plot.title = element_text(face = "bold", size = 16))
+ggsave ("~/Desktop/Berkeley/Classes15_16/PS239T-final-project/Results/Year_Graph.pdf", dpi = 200)
+dev.off()
